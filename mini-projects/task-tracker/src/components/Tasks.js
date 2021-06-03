4f8cb6ee9ad969dffcd654189dca2e875b75b8b6
Fernando74lr <flopezramirez@hotmail.com>
2021-06-03 00:52:58 -0500

    Task tracker

---
+++ mini-projects/task-tracker/src/components/Tasks.js
@@ -0,0 +1,15 @@
+import Task from "./Task";
+
+const Tasks = ({ tasks, onDelete, onToogle }) => {    
+    return (
+        <>
+            {
+                tasks.map((task, index) => (
+                    <Task key={index} task={task} onDelete={onDelete} onToogle={onToogle} />
+                ))
+            }
+        </>
+    );
+}
+
+export default Tasks;
