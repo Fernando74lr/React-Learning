be076557ac2765b6f6361957963155d087579a97
Fernando74lr <flopezramirez@hotmail.com>
2021-06-05 13:48:40 -0500

    02 - useState - Hook

--- react-course/02-counter-app/src/CounterApp.js
+++ react-course/02-counter-app/src/CounterApp.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const CounterApp = ({ value }) => {
 
-    const handleAdd = (e) => console.log(e);
+    const [counter, setCounter] = useState(0);
+    console.log('~ counter:', counter);
+
+    const handleAdd = () => {
+        setCounter(counter + 1);
+        // setCounter( (c) => c + 1); /* If for some reason we don't have acces to, in this case, 'counter' */
+    };
+
     return (
         <div>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
             <button onClick={ handleAdd }>+1</button>
         </div>
     )
