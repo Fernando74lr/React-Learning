36b79208dbe9a581609417f1a595742c18be993a
Fernando74lr <flopezramirez@hotmail.com>
2021-06-05 14:51:18 -0500

    Testing - Jeest - Expect - toBe - Group all the tests

--- react-course/02-counter-app/src/tests/demo.test.js
+++ react-course/02-counter-app/src/tests/demo.test.js
@@ -1,9 +1,13 @@
 
-
-test('should', () => {
-    const isActive = true;
-
-    if (isActive) {
-        throw new Error('It is not active');
-    }
-})
+describe('Tests in the demo.test.js file', () => {
+    test('both strings should be equal', () => {
+        // 1. Initialization
+        const msg2 = 'Hey!';
+        
+        // 2. Stimulus
+        const msg = 'Hey';
+    
+        // 4. Observe behavior
+        expect(msg).toBe(msg2); // ===
+    });
+});
