341795c5e7b7b36812ce3cc554d19a149b26d192
Fernando74lr <flopezramirez@hotmail.com>
2021-06-05 12:44:02 -0500

    02 - Return elements in the component and Fragment

--- react-course/02-counter-app/src/FirstApp.js
+++ react-course/02-counter-app/src/FirstApp.js
@@ -1,6 +1,12 @@
+// import { Fragment } from "react";
 
 const FirstApp = () => {
-    return <h1>Hello world !</h1>;
+    return (
+        <>
+            <h1>Hello world !</h1>
+            <p>My first application</p>
+        </>
+    );
 }
 
 export default FirstApp;
