3ef1a7d4dabb68003ad49a77bc07f4e04e4dbf6f
Fernando74lr <flopezramirez@hotmail.com>
2021-06-07 00:57:11 -0500

    Testing - SImulate 'click' events

--- react-course/02-counter-app/src/tests/CounterApp.test.js
+++ react-course/02-counter-app/src/tests/CounterApp.test.js
@@ -3,24 +3,28 @@ import { shallow } from 'enzyme';
 import '@testing-library/jest-dom';
 
 describe('Tests in <CounterApp />', () => {
+    const wrapper = shallow(<CounterApp />);
     test('should show <CounterApp /> correctly', () => {
-        const value = 50;
-        const wrapper = shallow(<CounterApp value={value} />);
         expect(wrapper).toMatchSnapshot();
     });
 
     test('should show subtitle sent by props', () => {
         const value = 100;
 
-        const wrapper = shallow(
-            <CounterApp 
-                value={value}
-            />
-        );
+        const wrapper = shallow( <CounterApp value={value} />);
 
         const textPara = wrapper.find('h2').text().trim();
 
-        expect(textPara.replace(' ', '')).toBe(value.toString());
+        expect(textPara).toBe(value.toString());
     });   
+
+    test('should increment counter in 1', () => {
+        wrapper.find('button').at(2).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('11');
+
+    })
+    
     
 });
