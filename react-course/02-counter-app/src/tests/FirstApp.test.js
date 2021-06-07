274388d5ed9233fb2336d4ff5ebbd58e68bc21a7
Fernando74lr <flopezramirez@hotmail.com>
2021-06-07 00:21:01 -0500

    Testing - Enzyme - Testing unit

--- react-course/02-counter-app/src/tests/FirstApp.test.js
+++ react-course/02-counter-app/src/tests/FirstApp.test.js
@@ -1,14 +1,22 @@
-import { render } from "@testing-library/react";
+// import { render } from "@testing-library/react";
+// import React from 'react';
 import FirstApp from "../FirstApp";
-import React from 'react';
+import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
 
 describe('Tests in <FirstApp />', () => {
-    test('should show the message: "I\'m Fernando"', () => {
+    // test('should show the message: "I\'m Fernando"', () => {
+    //     const greeting = 'I\'m Fernando';
+
+    //     const { getByText} = render(<FirstApp greeting={greeting} />);
+
+    //     expect(getByText(greeting)).toBeInTheDocument();
+    // });
+
+    test('should show <FirstApp /> correctly', () => {
         const greeting = 'I\'m Fernando';
-
-        const { getByText} = render(<FirstApp greeting={greeting} />);
-
-        expect(getByText(greeting)).toBeInTheDocument();
-    })
+        const wrapper = shallow(<FirstApp greeting={greeting} />);
+        expect(wrapper).toMatchSnapshot();
+    });    
     
 });
