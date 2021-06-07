b85b0af10b0a7e6bfe3b07955222bbb45f576d22
Fernando74lr <flopezramirez@hotmail.com>
2021-06-07 01:04:52 -0500

    Testing - Tests with the reset button

--- react-course/02-counter-app/src/tests/CounterApp.test.js
+++ react-course/02-counter-app/src/tests/CounterApp.test.js
@@ -3,7 +3,9 @@ import { shallow } from 'enzyme';
 import '@testing-library/jest-dom';
 
 describe('Tests in <CounterApp />', () => {
+
     const wrapper = shallow(<CounterApp />);
+
     test('should show <CounterApp /> correctly', () => {
         expect(wrapper).toMatchSnapshot();
     });
@@ -16,13 +18,30 @@ describe('Tests in <CounterApp />', () => {
         const textPara = wrapper.find('h2').text().trim();
 
         expect(textPara).toBe(value.toString());
-    });   
+    });
 
     test('should increment counter in 1', () => {
+        wrapper.find('button').at(0).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('1');
+
+    });
+
+    test('should decrement counter in 1', () => {
         wrapper.find('button').at(2).simulate('click');
         const counterText = wrapper.find('h2').text().trim();
 
-        expect(counterText).toBe('11');
+        expect(counterText).toBe('0');
+
+    });
+
+    test('should put the default value with the btn reset', () => {
+        const wrapper = shallow(<CounterApp value={ 50 } />);
+        wrapper.find('button').at(1).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('50');
 
     })
     
