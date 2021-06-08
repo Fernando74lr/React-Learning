a6b888e60fb501f107e2f8f3cb596eaf847b2c78
Fernando74lr <flopezramirez@hotmail.com>
2021-06-08 13:20:11 -0500

    03 - Creating a category list

--- react-course/03-gif-expert-app/src/components/GifExpertApp.js
+++ react-course/03-gif-expert-app/src/components/GifExpertApp.js
@@ -1,11 +1,31 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 
 const GifExpertApp = () => {
+
+    // const categories = ['Friends', 'Dr. House', 'Dexter'];
+    const [categories, setCategories] = useState([
+        'Friends',
+        'Dr. House',
+        'Dexter'
+    ]);
+
+    const handleAdd = (newSerie) => {
+        setCategories(cat => [...cat, newSerie]);
+    }
+
     return (
         <>
            <h2>GifExpertApp</h2>
            <hr />
+           <button onClick={ () => handleAdd('The Simpsons') }>Add serie</button>
+           <ol>
+               {
+                   categories.map(cat => {
+                        return <li key={cat}>{cat}</li>
+                   })
+               }
+           </ol>
         </>
     )
 }
