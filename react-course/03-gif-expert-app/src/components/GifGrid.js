f2cdc39a529ebe6021fb3ec9bd0091af630f2bf7
Fernando74lr <flopezramirez@hotmail.com>
2021-06-14 15:31:12 -0500

    03 - Helpers - getGifs()

--- react-course/03-gif-expert-app/src/components/GifGrid.js
+++ react-course/03-gif-expert-app/src/components/GifGrid.js
@@ -1,5 +1,6 @@
 
 import React, { useState, useEffect }from 'react';
+import { getGifs } from '../helpers/getGifs';
 import GifGridItem from './GifGridItem';
 
 const GifGrid = ({ category }) => {
@@ -12,26 +13,9 @@ const GifGrid = ({ category }) => {
         this component is rendered.
     */
     useEffect( () => {
-        getGifs();
-    }, []);
-
-    const getGifs = async() => {
-
-        const url = `http://api.giphy.com/v1/gifs/search?q=Dr. House&limit=10&api_key=iPlnzzkiQMdEmc7mA81SQAqz53frGnAQ`;
-        const response = await fetch(url);
-        const {data} = await response.json();
-        const gifs = data.map(gif => {
-            return {
-                id: gif.id,
-                title: gif.title,
-                url: gif.images?.downsized_medium.url
-            }
-        })
-        console.log('~ gifs', gifs);
-
-        setImages(gifs);
-        
-    }
+        getGifs(category)
+            .then(gifs => setImages(gifs)); // or just .then(gifs => setImages);
+    }, [category]);
 
     return (
         <>
---
+++ react-course/03-gif-expert-app/src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+
+
+export const getGifs = async(category) => {
+
+    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=iPlnzzkiQMdEmc7mA81SQAqz53frGnAQ`;
+    const response = await fetch(url);
+    const {data} = await response.json();
+    const gifs = data.map(gif => {
+        return {
+            id: gif.id,
+            title: gif.title,
+            url: gif.images?.downsized_medium.url
+        }
+    })
+    console.log('~ gifs', gifs);
+
+   return gifs
+}
