15dccab60599a5158b67c11ed9592b34579e6479
Fernando74lr <flopezramirez@hotmail.com>
2021-06-14 15:53:11 -0500

    03 - Custom Hook

--- react-course/03-gif-expert-app/src/components/GifGrid.js
+++ react-course/03-gif-expert-app/src/components/GifGrid.js
@@ -1,33 +1,19 @@
 
 import React from 'react';
 import useFetchGifs from '../hooks/useFetchGifs';
-// import { getGifs } from '../helpers/getGifs';
-// import GifGridItem from './GifGridItem';
+import GifGridItem from './GifGridItem';
 
 const GifGrid = ({ category }) => {
 
-    // const [images, setImages] = useState([]);
-
-    const { data, loading } = useFetchGifs();
-    console.log('~ loading', loading);
-    console.log('~ data', data);
-
-    /*
-        This means I want to execute whatever
-        is inside only the first time
-        this component is rendered.
-    */
-    // useEffect( () => {
-    //     getGifs(category)
-    //         .then(gifs => setImages(gifs)); // or just .then(gifs => setImages);
-    // }, [category]);
+    const { data:images, loading } = useFetchGifs(category);
 
     return (
         <>
             <h3>{ category }</h3>
 
-            { loading ? 'Loading...' : 'Data received!'}
-            {/* <div className="card-grid">
+            { loading && <h4>Loading...</h4> }
+            
+            <div className="card-grid">
                 {
                     images.map(img => (
                         <GifGridItem
@@ -37,7 +23,7 @@ const GifGrid = ({ category }) => {
                         />
                     ))
                 }
-            </div> */}
+            </div>
         </>
     )
 }
--- react-course/03-gif-expert-app/src/hooks/useFetchGifs.js
+++ react-course/03-gif-expert-app/src/hooks/useFetchGifs.js
@@ -1,17 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
+import { getGifs } from '../helpers/getGifs';
 
- const useFetchGifs = () => {
+ const useFetchGifs = (category) => {
     const [state, setstate] = useState({
         data: [],
         loading: true
     });
 
-    setTimeout(() => {
-        setstate({
-            data: [1, 2, 3, 4, 5],
-            loading: false
-        });
-    }, 3000);
+    /*
+        This means I want to execute whatever
+        is inside only the first time
+        this component is rendered.
+    */
+    useEffect( () => {
+        getGifs(category)
+            .then( imgs => {
+                setTimeout(() => {
+                    setstate({
+                        data: imgs,
+                        loading: false
+                    });
+                }, 1500);
+            });
+    }, [category]);
 
     return state;
  }
