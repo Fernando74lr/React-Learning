5ed67d49376e11bce5c5b58b7a17750c4bad1030
Fernando74lr <flopezramirez@hotmail.com>
2021-06-24 15:14:21 -0500

    useEffect - Precautions

--- react-course/04-hook-app/src/components/02-useEffect/Message.jsx
+++ react-course/04-hook-app/src/components/02-useEffect/Message.jsx
@@ -2,18 +2,30 @@ import { useState, useEffect } from 'react';
 
 const Message = () => {
 
+    const [coords, setCoords] = useState({ x:0, y:0 })
+    const { x, y } = coords;
+
     useEffect(() => {
 
-        console.log('Component mounted');
+        const mouseMove = (e) => {
+            const coords = { x: e.x, y: e.y };
+            setCoords(coords);
+        };
+
+        window.addEventListener('mousemove', mouseMove);
         
         return () => {
-            console.log('Component unmounted');
+            // Cleanup
+            window.addEventListener('mousemove', mouseMove);
         }
     }, []);
 
     return (
         <>
-            <h1>You're cool.</h1> 
+            <h1>You're cool.</h1>
+            <p>
+                x: { x}, y: { y }
+            </p>
         </>
     )
 }
