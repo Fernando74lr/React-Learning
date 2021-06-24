31ac16079d0a64fe14dc36f5c66ce5748ddcbac0
Fernando74lr <flopezramirez@hotmail.com>
2021-06-24 14:45:58 -0500

    04 - useEffect - SimpleForm

---
+++ react-course/04-hook-app/src/components/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react';
+import './effects.css';
+
+const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        name: '',
+        email: ''
+    });
+
+    const { name, email } = formState;
+
+    useEffect( () => {
+        // console.log('Hey!');
+    }, []);
+
+    useEffect( () => {
+        // console.log('formState cambió');
+    }, [formState]);
+
+    useEffect( () => {
+        // console.log('Email cambió');
+    }, [email]);
+
+    const handleInputChange = ({ target }) => {
+        setFormState({
+            ...formState,
+            [target.name]: target.value
+        });
+    }
+
+    return (
+        <>
+            <h1>useEffect</h1>
+            <hr />
+
+            {/* Name */}
+            <div className="form-group">
+                <input
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    placeholder="Tu nombre"
+                    autoComplete="off"
+                    value={ name }
+                    onChange={ handleInputChange }
+                />
+            </div>
+
+            {/* Email */}
+            <div className="form-group">
+                <input
+                    type="text"
+                    name="email"
+                    className="form-control"
+                    placeholder="test@gmail.com"
+                    autoComplete="off"
+                    value={ email }
+                    onChange={ handleInputChange }
+                />
+            </div>
+        </>
+    )
+}
+
+export default SimpleForm
