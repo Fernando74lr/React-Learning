f6d6716db572fee97abd19ccc87c5bbc2cd4b180
Fernando74lr <flopezramirez@hotmail.com>
2021-06-30 11:57:43 -0500

    04 - useFetch - Custom Hook

--- react-course/04-hook-app/src/index.js
+++ react-course/04-hook-app/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
 // import SimpleForm from './components/02-useEffect/SimpleForm';
 // import CounterApp from './components/01-useState/CounterApp';
 // import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
 // import HooksApp from './HooksApp';
-import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
+// import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <FormWithCustomHook />
+    <MultipleCustomHooks />
   </React.StrictMode>,
   document.getElementById('root')
 );
