78e1fd258b779d466e5b1ad8c884b4231132c6ea
Fernando74lr <flopezramirez@hotmail.com>
2021-06-30 12:52:03 -0500

    04 - useRef - First use

--- react-course/04-hook-app/src/index.js
+++ react-course/04-hook-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
+import FocusScreen from './components/04-useRef/FocusScreen';
+// import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
 // import SimpleForm from './components/02-useEffect/SimpleForm';
 // import CounterApp from './components/01-useState/CounterApp';
 // import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
@@ -10,7 +11,7 @@ import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
   </React.StrictMode>,
   document.getElementById('root')
 );
