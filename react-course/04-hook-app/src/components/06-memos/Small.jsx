8f1eaa36ff62f0a166ada44e0006fa28588184db
Fernando74lr <flopezramirez@hotmail.com>
2021-07-02 13:32:59 -0500

    04 - Memo - React Method

--- react-course/04-hook-app/src/components/06-memos/Small.jsx
+++ react-course/04-hook-app/src/components/06-memos/Small.jsx
@@ -1,5 +1,6 @@
 import { memo } from "react";
 
+// Memo: memorize something, only if its properties have changed it'll be rendered again.
 const Small = memo(({ value }) => {
 
     console.log('Me volví a mostrar xd');
