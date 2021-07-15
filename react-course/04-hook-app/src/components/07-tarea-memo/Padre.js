c03af19cd61443c6ead70eea5310d1c15c6a7a7e
Fernando74lr <flopezramirez@hotmail.com>
2021-07-15 13:46:32 -0500

    Changes

--- react-course/04-hook-app/src/components/07-tarea-memo/Padre.js
+++ react-course/04-hook-app/src/components/07-tarea-memo/Padre.js
@@ -30,7 +30,7 @@ export const Padre = () => {
                         numero={ n }
                         incrementar={ incrementar }
                     />
-                ))
+                )) 
             }
             {/* <Hijo /> */}
         </div>
