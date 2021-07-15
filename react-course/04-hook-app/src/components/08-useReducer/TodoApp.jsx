8b44dcebb7369645306652ce36d7aa324108e019
Fernando74lr <flopezramirez@hotmail.com>
2021-07-15 14:36:11 -0500

    04 - Creating shell of thee TODOs list

--- react-course/04-hook-app/src/components/08-useReducer/TodoApp.jsx
+++ react-course/04-hook-app/src/components/08-useReducer/TodoApp.jsx
@@ -15,14 +15,46 @@ const TodoApp = () => {
     
     return (
         <div>
-            <h1>TodoApp</h1>
+            <h1>TodoApp ( { todos.length } )</h1>
             <hr />
 
-            <ul>
-                <li>Hola</li>
-                <li>Hola Mundo</li>
-                <li>Qué hay</li>
-            </ul>
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group list-group-flush">
+                        {
+                            todos.map((task, i) => (
+                                <li
+                                    key={ task.id }
+                                    className="list-group-item">
+                                    <p className="text-center">
+                                        { i + 1}. { task.desc }
+                                    </p>
+                                    <button className="btn btn-danger">
+                                        Delete
+                                    </button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4>Add To Do</h4>
+                    <hr />
+                    
+                    <form>
+                        <input
+                            type="text"
+                            name="description"
+                            className="form-control"
+                            placeholder="Learn..."
+                            autoComplete="off"
+                        />
+                        <button className="btn btn-outline-primary mt-1 btn-block">
+                            Add
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
