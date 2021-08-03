d83d923427d1a24ff0ef3040ab8cbd90d93b544b
Fernando74lr <flopezramirez@hotmail.com>
2021-08-03 13:00:17 -0500

    04 - CreateContext and useContext

--- react-course/04-hook-app/src/components/09-useContext/MainApp.jsx
+++ react-course/04-hook-app/src/components/09-useContext/MainApp.jsx
@@ -1,10 +1,20 @@
 
 import React from 'react';
 import AppRouter from './AppRouter';
+import { UserContext } from './UserContext';
 
 const MainApp = () => {
+
+    const user = {
+        id: 1234,
+        name: 'Fernando',
+        email: 'flopezramirez@hotmail.com'
+    };
+
 	return (
-		<AppRouter />
+        <UserContext.Provider value={ user }>
+            <AppRouter />
+        </UserContext.Provider>
 	);
 }
 
