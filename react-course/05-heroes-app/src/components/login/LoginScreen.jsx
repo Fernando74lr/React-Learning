b4e7c4a81cd29ac274ff9427653ee9cb487122b5
Fernando74lr <flopezramirez@hotmail.com>
2021-08-19 00:03:37 -0500

    05 - History push / replace

--- react-course/05-heroes-app/src/components/login/LoginScreen.jsx
+++ react-course/05-heroes-app/src/components/login/LoginScreen.jsx
@@ -1,8 +1,23 @@
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ history }) => {
+
+    const handleLogin = () => {
+        console.log("Click");
+        // history.push('/'); // Te redirecciona a la ruta que quieras.
+        history.replace('/'); // Hace un replace de la historia
+    }
+
     return (
-        <div>
+        <div className="container mt-5">
             <h1>Login Screen</h1>
+            <hr />
+
+            <button
+                className="btn btn-primary"
+                onClick={ handleLogin }
+            >
+              Login  
+            </button>
         </div>
     );
 }
