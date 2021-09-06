1d6e0615ac3a2e5c5ca0300c43eef2815e63af7b
Fernando74lr <flopezramirez@hotmail.com>
2021-09-06 00:33:59 -0500

    06 - Register Styles

--- react-course/06-journal-app/src/components/auth/RegisterScreen.jsx
+++ react-course/06-journal-app/src/components/auth/RegisterScreen.jsx
@@ -1,8 +1,55 @@
+import { Link } from "react-router-dom";
 
 export const RegisterScreen = () => {
     return (
-        <div>
-            <h1>RegisterScreen</h1>
-        </div>
+        <>
+            <h3 className="auth__title">Register</h3>
+
+            <form action="">
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    className="auth__input"
+                    autoComplete="off"
+                />  
+
+                <input
+                    type="text"
+                    name="email"
+                    placeholder="Email"
+                    className="auth__input"
+                    autoComplete="off"
+                />
+
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    className="auth__input"
+                />
+
+                <input
+                    type="password"
+                    name="password2"
+                    placeholder="Confirm"
+                    className="auth__input"
+                />
+
+                <button
+                    className="btn btn-primary btn-block mb-5"
+                    type="submit"
+                >
+                    Register
+                </button>
+
+                <Link
+                    to="/auth/login"
+                    className="link"
+                >
+                    Already registered?
+                </Link>
+            </form>
+        </>
     );
 }
