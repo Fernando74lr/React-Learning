f9907ed391ccf6ba7e3e18b037fe1e8a5e249c2c
Fernando74lr <flopezramirez@hotmail.com>
2021-09-07 16:44:32 -0500

    06 - Handle Errors in register form

--- react-course/06-journal-app/src/components/auth/RegisterScreen.jsx
+++ react-course/06-journal-app/src/components/auth/RegisterScreen.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
 import { useForm } from "../../hooks/useForm";
+import validator from 'validator';
+import { toast } from "../../helpers/sweetAlert2";
 
 export const RegisterScreen = () => {
 
     // Form Hook
     const [ formValues, handleInputChange] = useForm({
-        name: '',
-        email: '',
-        password: '',
-        password2: ''
+        name: 'Fernando',
+        email: 'lopezramirez330@gmail.com',
+        password: '12345',
+        password2: '12345'
     });
 
     // Extract data
@@ -17,12 +19,25 @@ export const RegisterScreen = () => {
     // Handle register
     const handleRegister = (e) => {
         e.preventDefault();
+        if (isFormValid()) {
+            console.log({ name, email, password, password2 });
+        }
 
-        console.log({ name, email, password, password2 });
     }
 
     const isFormValid = () => {
-        // TODO        
+        if (name.trim().length === 0) {
+            toast('error', 'Invalid name.');
+            return false;
+        } else if (!validator.isEmail(email)) {
+            toast('error', 'Invalid email.');
+            return false;
+        } else if (password !== password2 || password.length < 5) {
+            toast('error', 'Invalid password. It should be at least 6 characters and match each others.');
+            return false;
+        }
+
+        return true;
     }
 
     return (
@@ -30,6 +45,11 @@ export const RegisterScreen = () => {
             <h3 className="auth__title">Register</h3>
 
             <form onSubmit={ handleRegister }>
+
+                <div className="auth__alert-error">
+                    Hola, mundo
+                </div>
+
                 <input
                     type="text"
                     name="name"
