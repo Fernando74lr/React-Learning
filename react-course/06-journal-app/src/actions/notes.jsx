8c9237b9b2391edc98383f4c0ab66dfe671ccd3a
Fernando74lr <flopezramirez@hotmail.com>
2021-09-28 20:24:33 -0500

    06 - Activate the created note

--- react-course/06-journal-app/src/actions/notes.jsx
+++ react-course/06-journal-app/src/actions/notes.jsx
@@ -1,18 +1,27 @@
 import { addDoc, collection } from "@firebase/firestore";
+import { types } from '../types/types';
 import { db } from "../firebase/firebase-config";
 
 export const startNewNote = () => {
-    return async ( dispatch, getState ) => {
-        const { uid } = getState().auth;
+	return async ( dispatch, getState ) => {
+		const { uid } = getState().auth;
 
-        const newNode = {
-            title: '',
-            body: '',
-            date: new Date().getTime()
-        }
+		const newNode = {
+			title: '',
+			body: '',
+			date: new Date().getTime()
+		}
 
-        const doc = await addDoc(collection(db, `${uid}`, "journal/notes"), newNode);
+		const doc = await addDoc(collection(db, `${uid}`, "journal/notes"), newNode);
 
-        console.log("Document written with ID: ", doc);
-    }
+		dispatch(activeNote(doc.id, newNode));
+	}
 }
+
+export const activeNote = ( id, note ) => ({
+	type: types.notesActive,
+	payload: {
+		id,
+		...note
+	}
+})
